0658f4dff020cfeb850d13ab25a646b1ebddf238
BMTmohammedtaha <bmt.mohammedtaha@gmail.com>
2023-12-25 14:17:24 -0800

    add MailerException used for throw Exception in MailerInterface::send method

---
+++ src/Exception/MailerException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Effectra\Mail\Exception;
+
+/**
+ * Class MailerException
+ *
+ * Represents an exception related to mailer functionality.
+ */
+class MailerException extends \Exception
+{
+
+}
